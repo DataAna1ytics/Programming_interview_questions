6d84646ff74005eda488f0627be6711e5bef9b3c
Ilia Gavrilov <elijahgavri@gmail.com>
2018-01-11 12:48:36 +0300

    Update Prime_numbers_generator_in_Python.py

--- Greenpeace_Prime_numbers/Prime_numbers_generator_in_Python.py
+++ Greenpeace_Prime_numbers/Prime_numbers_generator_in_Python.py
@@ -4,7 +4,7 @@ Created on Fri Aug  4 13:48:14 2017
 
 @author: Gavrilov
 """
-#Position: Senior data analyst
+#Position: Senior business data analyst
 #Task: Implement code to display prime numbers up to 100 in any programming language
 #On-site interview
 
